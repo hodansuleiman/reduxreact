6aac614a952553001f76a3919c3a59c560043751
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-05-22 19:25:01 -0400

    toggle selected breeds

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectBreeds } from "../features/breedsSlice";
 
 const Breeds = () => {
+  const [selectedBreeds, setselectedBreeds] = useState([]);
+
   const data = useSelector(selectBreeds);
+
+  const handleClick = (e) => {
+    const value = e.target.dataset.breed;
+    let _selectedBreeds = [];
+    if (selectedBreeds.indexOf(value) > -1) {
+      const _selectedBreeds = selectedBreeds.filter((breed) => breed !== value);
+      _setselectedBreeds(_selectedBreeds);
+    } else {
+      const _selectedBreeds = [...selectedBreeds, value];
+    }
+    setselectedBreeds(_selectedBreeds);
+  };
 
   const breeds =
     data &&
     data.map((breed) => {
-      return <li>{breed}</li>;
+      return (
+        <li data-breed={breed} key={breed} onClick={handleClick}>
+          {breed}
+        </li>
+      );
     });
 
   return;
