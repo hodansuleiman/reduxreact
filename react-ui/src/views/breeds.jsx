8fd3d0a5938d1e6dc71d03692eb3cc88f83a6d7b
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-05-22 19:03:30 -0400

    show breeds at/breeds

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectBreeds } from "../features/breedsSlice";
 
 const Breeds = () => {
-  return (
+  const data = useSelector(selectBreeds);
+
+  const breeds =
+    data &&
+    data.map((breed) => {
+      return <li>{breed}</li>;
+    });
+
+  return;
+  <>
     <h1>Breeds</h1>
-  )
+    <ul>{breeds}</ul>
+  </>;
 };
 
 export default Breeds;
